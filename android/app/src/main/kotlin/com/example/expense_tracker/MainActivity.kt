41b2524000c984af75a786ff3715d364e664558f
Xingkkk091 <stu70020@gmail.com>
2026-05-17 13:40:32 +0800

    v1.1.2: fix biometric - MainActivity extends FlutterFragmentActivity + USE_BIOMETRIC permission

--- android/app/src/main/kotlin/com/example/expense_tracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/expense_tracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.expense_tracker
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+// 必須繼承 FlutterFragmentActivity，否則 local_auth 的 BiometricPrompt 會閃退/無反應
+class MainActivity : FlutterFragmentActivity()
